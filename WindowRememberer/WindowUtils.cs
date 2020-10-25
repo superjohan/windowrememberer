ed4c0115eaa503bfb57a0ac1ce18ce9b254afa03
Johan Halin <johan@halin.me>
2020-10-25 18:00:25 +0200

    Functions for getting the current screen and setting a rect for the current window.

--- WindowRememberer/WindowUtils.cs
+++ WindowRememberer/WindowUtils.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace WindowRememberer
 {
@@ -13,11 +14,31 @@ namespace WindowRememberer
         [DllImport("user32.dll", SetLastError = true)]
         public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int Width, int Height, bool Repaint);
 
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
         public static Process[] WindowedProcesses()
         {
             Process[] processlist = Process.GetProcesses();
 
             return processlist.Where(process => !string.IsNullOrEmpty(process.MainWindowTitle)).ToArray();
+        }
+
+        public static Screen CurrentWindowScreen()
+        {
+            var handle = GetForegroundWindow();
+
+            return Screen.FromHandle(handle);
+        }
+
+        public static void SetCurrentWindowRect(Rect rect)
+        {
+            var handle = GetForegroundWindow();
+
+            if (!MoveWindow(handle, rect.left, rect.top, rect.width, rect.height, true))
+            {
+                Debug.WriteLine("Could not set rect for window.");
+            }
         }
     }
 }
