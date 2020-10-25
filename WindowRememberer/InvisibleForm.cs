8a21e22faabd5775afcd448e2eca67b41274e945
Johan Halin <johan@halin.me>
2020-10-25 17:05:57 +0200

    Register a hot key handler in the form and forward events to it.

--- WindowRememberer/InvisibleForm.cs
+++ WindowRememberer/InvisibleForm.cs
@@ -12,6 +12,10 @@ namespace WindowRememberer
 {
     public partial class InvisibleForm : Form
     {
+        private const int WM_HOTKEY = 0x312;
+
+        private HotKeyHandler hotKeyHandler;
+
         public InvisibleForm()
         {
             InitializeComponent();
@@ -22,7 +26,19 @@ namespace WindowRememberer
             Visible = false;
             ShowInTaskbar = false;
 
+            hotKeyHandler = new HotKeyHandler(Handle);
+
             base.OnLoad(e);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_HOTKEY)
+            {
+                hotKeyHandler.OnHotKeyPressed(m.LParam);
+            }
+
+            base.WndProc(ref m);
         }
     }
 }
