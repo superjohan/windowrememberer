db28b7ae53a177cc7d1bc38e0f6d87cae62a87bc
Johan Halin <johan@halin.me>
2020-10-25 17:15:56 +0200

    Extract Rect struct to its own file.

---
+++ WindowRememberer/Rect.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+namespace WindowRememberer
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Rect
+    {
+        public int left;
+        public int top;
+        public int right;
+        public int bottom;
+
+        public int width
+        {
+            get
+            {
+                return right - left;
+            }
+        }
+
+        public int height
+        {
+            get
+            {
+                return bottom - top;
+            }
+        }
+
+        override public string ToString()
+        {
+            return "(x: " + left + ", y: " + top + ", width: " + width + ", height: " + height + ")";
+        }
+    }
+}
--- WindowRememberer/WindowPropertyManager.cs
+++ WindowRememberer/WindowPropertyManager.cs
@@ -8,36 +8,6 @@ namespace WindowRememberer
 {
     class WindowPropertyManager
     {
-        [StructLayout(LayoutKind.Sequential)]
-        public struct Rect
-        {
-            public int left;
-            public int top;
-            public int right;
-            public int bottom;
-
-            public int width
-            {
-                get
-                {
-                    return right - left;
-                }
-            }
-
-            public int height
-            {
-                get
-                {
-                    return bottom - top;
-                }
-            }
-
-            override public string ToString()
-            {
-                return "(x: " + left + ", y: " + top + ", width: " + width + ", height: " + height + ")";
-            }
-        }
-
         [DllImport("user32.dll", SetLastError = true)]
         static extern bool GetWindowRect(IntPtr hWnd, ref Rect Rect);
 
