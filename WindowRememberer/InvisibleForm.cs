3743ccf99203c7e43fb1b4e5fd4a4ee95281aeff
Johan Halin <johan@halin.me>
2020-10-25 15:41:19 +0200

    Add an invisible form to help with getting hot key events.

---
+++ WindowRememberer/InvisibleForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowRememberer
+{
+    public partial class InvisibleForm : Form
+    {
+        public InvisibleForm()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false;
+            ShowInTaskbar = false;
+
+            base.OnLoad(e);
+        }
+    }
+}
